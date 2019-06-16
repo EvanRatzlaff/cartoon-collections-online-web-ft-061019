23e5e9b24209fc4146fe2893333a03d9f6082ae2
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-16 23:22:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ require "pry"
 def roll_call_dwarves(array)
   fourdwarves = ["Doc","Dopey","Bashful", "Grumpy"]
   i = 0 
-  while 
+  while i < array.length 
+  
 end
 binding.pry 
 def summon_captain_planet# code an argument here
